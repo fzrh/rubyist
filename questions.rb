749f8df22b78ad8631b01b452bb4cc8310d9bfc2
Faezrah Rizalman <fwrizalman@gmail.com>
2014-07-04 13:44:27 +0100

    Answered 12 out of 39 questions

--- questions.rb
+++ questions.rb
@@ -1,22 +1,35 @@
 # keep only the elements that start with an a
 def select_elements_starting_with_a(array)
+  array.select do |n|
+    n.start_with?('a')
+  end
 end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
+  array.select do |n|
+    n.start_with?('a', 'e','i', 'o', 'u')
+  end
 end
 
 # remove instances of nil (but NOT false) from an array
 def remove_nils_from_array(array)
+  array.compact
 end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
+  array.compact.reject do |n|
+    n == false 
+  end
 end
 
 # don't reverse the array, but reverse every word inside it. e.g.
 # ['dog', 'monkey'] becomes ['god', 'yeknom']
 def reverse_every_element_in_array(array)
+  array.map do |n|
+    n.reverse
+  end
 end
 
 # given an array of student names, like ['Bob', 'Dave', 'Clive']
@@ -29,10 +42,12 @@ end
 # discard the first 3 elements of an array, 
 # e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
 def all_elements_except_first_3(array)
+  array.drop(3)
 end
 
 # add an element to the beginning of an array
 def add_element_to_beginning_of_array(array, element)
+  array.unshift(element)
 end
 
 # sort an array of words by their last letter, e.g.
@@ -56,6 +71,9 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
+  even = array.select {|e| e.even?}
+  odd = array.select {|e| e.odd?}
+  [even, odd]
 end
 
 # count the numbers of elements in an element which are palindromes
@@ -76,15 +94,22 @@ end
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
 def total_of_array(array)
+  sum = 0
+  array.each do |n|
+    sum +=n
+  end
+  sum
 end
 
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
 def double_array(array)
+  [array , array].flatten
 end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
+  symbol.to_s
 end
 
 # get the average from an array, rounded to the nearest integer
@@ -110,6 +135,7 @@ end
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
 def get_all_letters_in_array_of_words(array)
+    array.join.split(//).sort
 end
 
 # swap the keys and values in a hash. e.g.
